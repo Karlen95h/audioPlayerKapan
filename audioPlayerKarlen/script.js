cf8ffb02799c41296672bae095984e443c7f511c
Karlen <85682913+Karlen95h@users.noreply.github.com>
2024-05-14 17:17:55 +0400

    Update script.js

--- audioPlayerKarlen/script.js
+++ audioPlayerKarlen/script.js
@@ -27,7 +27,7 @@ var data = {
 
 
 let song  =  new Audio()
-
+console.log(song);
 window.onload  = function (){
     playSong()
 }
@@ -59,18 +59,77 @@ function playOrPauseSong(){
 }
 
 
+song.addEventListener("timeupdate",function (){
+        //  console.log(song.currentTime);
+        //  console.log(song.duration);
 
-song.addEventListener("timeupdate" , function (){
-            let fill = document.getElementsByClassName("fill")
-                // console.log(song.currentTime);
-                // console.log(song.duration);
+        let fill = document.getElementsByClassName("fill")
+        let position = song.currentTime /  song.duration 
+        fill[0].style.marginLeft = position * 100 + "%"
+       
+        convertTime(song.currentTime)
 
-                let position = song.currentTime / song.duration //1
-                fill[0].style.marginLeft = position * 99 + "%"
+        if(song.ended){
+                next()
+        }
+        
+})
 
 
-                
 
-} )
+function   convertTime(seconds){
+            currentTime = document.getElementsByClassName("currentTime")
+
+            let min  = Math.floor(seconds/60)
+            let sec  = Math.floor(seconds%60)
 
 
+
+           min = (min < 10) ? "0" + min : min;
+           sec = (sec < 10) ? "0" + sec : sec;
+
+           currentTime[0].textContent = min + ":" + sec
+
+           totalTime(song.duration)
+
+
+}
+
+
+
+function  totalTime(seconds){
+            
+       let min = Math.floor(seconds /60)
+       let sec = Math.floor(seconds %60)
+
+       min = (min < 10) ? "0" + min : min;
+       sec = (sec < 10) ? "0" + sec : sec;
+
+
+       currentTime[0].textContent  += " / "   + min + ":" + sec
+
+
+
+}
+
+
+function  next(){
+          currentSong++
+
+          if(currentSong >= data.song.length){
+                    currentSong = 0
+          }
+
+          playSong()
+}
+
+
+function  prev(){
+            currentSong--
+
+            if(currentSong  < 0){
+                    currentSong = data.song.length - 1 
+            }
+
+            playSong()
+}
